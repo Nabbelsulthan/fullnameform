5affc8d34f002b6fdd3f16008fb3cb1a6e528608
NABBEL SULTHAN <nabbelmech@gmail.com>
2026-01-08 14:43:12 +0530

    final display name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [firstName, setFirstName] = useState("")
+
+  const [lastName, setLastName] = useState("")
+
+
+
+  const handleSumbit = (event) => {
+
+    event.preventDefault();
+
+    alert(`Full Name: ${firstName} ${lastName}`)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="display-name">
+
+    <h1>Full Name Display</h1>
+   
+    <form onSubmit={handleSumbit}>
+      <label>First Name:</label>
+      <input type='text' id='fname' name='fname' value={firstName}   onChange={(event) => setFirstName(event.target.value)} required></input>
+      <br></br>
+
+      <label>Last Name:</label>
+      <input type='text' id='lname' name='lname' value={lastName} onChange={(event)=> setLastName(event.target.value)} required></input>
+      <br></br>
+
+      <button type='submit'>
+        submit
+      </button>
+
+      <br></br>
+
+        <label>
+          Full Name: {firstName} {lastName}
+        </label>
+    
+    </form>
+
     </div>
   );
 }
